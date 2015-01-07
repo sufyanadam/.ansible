af11e63e00eb5eb77fbff42bf195f8b4d477ac0f
Sufyan Adam <sufyan.adam@gmail.com>
2015-01-06 23:19:30 -0800

    update local version of ansible repo if already cloned

--- workstation/shell_scripts/install.sh
+++ workstation/shell_scripts/install.sh
@@ -25,4 +25,13 @@ if [[ ! -d $ANSIBLE_DIR ]]; then
     cd $ANSIBLE_DIR
 fi
 
+# If Ansible repo exists, get latest version
+
+if [[ -d $ANSIBLE_DIR ]]; then
+    git clone https://github.com/sufyanadam/.ansible
+    cd $ANSIBLE_DIR
+else
+    cd $ANSIBLE_DIR && git pull
+fi
+
 ansible-playbook --ask-sudo-pass ~/.ansible/site.yml -i $ANSIBLE_DIR/workstation/inventory
