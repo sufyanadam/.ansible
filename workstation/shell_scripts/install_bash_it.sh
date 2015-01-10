c77e6beb7a66d7c2de417faa7cbdee8b895300fa
Sufyan Adam <sufyan.adam@gmail.com>
2015-01-09 22:53:53 -0800

    try different approach

---
+++ workstation/shell_scripts/install_bash_it.sh
@@ -0,0 +1,10 @@
+export PATH="/usr/local/bin:$PATH"
+
+# Set path to bash it dir
+export BASH_IT="~/.bash_it"
+
+# Set bash it theme
+export BASH_IT_THEME="bobby"
+
+# Load bash-it
+source $BASH_IT/bash_it.sh
